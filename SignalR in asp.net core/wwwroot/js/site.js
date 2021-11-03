372340be4cbf608fcb6340c573f3ff1a7528fee3
Mohamed orabi <mohamed.orabi97@outlook.com>
2021-11-03 20:42:18 +0200

    hi

---
+++ SignalR in asp.net core/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+connection.on("ReceiveMessage", function (fromUser, message) {
+    var messages = fromUser + ":" + message;
+    var li = document.createElement("li");
+    li.textContent = messages;
+    $("#lists").append(li);
+})
+
+
+connection.start();
+
+$("#btnSubmit").on("click", function () {
+    var from = $("#txtUser").val();
+    var message = $("#txtMessage").val();
+
+    connection.invoke("SendMessage", from, message);
+})
